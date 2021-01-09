934b1697d80091756e1e15f8f9984845de969b22
engyahmednasr <77208180+engyahmednasr@users.noreply.github.com>
2021-01-09 23:05:13 +0200

    Create wheredropping.m

---
+++ wheredropping.m
@@ -0,0 +1,16 @@
+%This function is for recieving where the player will drop and if he can drop here or not and to check if he chose a right number between 1&7
+function chosencolumn=wheredropping(drawnboard, playerplaying)
+fprintf('%s turn now\n',playerplaying)
+%next is where he will drop
+chosencolumn=input('Enter a column number you wish to drop in: ');
+while chosencolumn < 1 || chosencolumn > 7
+    chosencolumn=input('Enter a column number between 1 & 7 please: ');
+end
+%next is checking if the column he/she chose is already full
+while drawnboard(1, chosencolumn)=='B' || drawnboard(1, chosencolumn)=='Y'
+    chosencolumn=input('This column is full please enter another column number: ');
+    while chosencolumn < 1 || chosencolumn > 7
+        chosencolumn=input('Enter a column number between 1 & 7 please: ');
+    end
+end
+end
