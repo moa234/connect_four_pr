ca3bb5618f208783d76a59bbdac585afd6a623a9
engyahmednasr <77208180+engyahmednasr@users.noreply.github.com>
2021-01-11 20:37:11 +0200

    Update reloadboard.m

--- reloadboard.m
+++ reloadboard.m
@@ -1,9 +1,9 @@
 %This function is to reload board everytime
-function theboard=reloadboard()
-theboard=xlsread('theboard.xlsx');
+function board=reloadboard()
+board=xlsread('board.xlsx');
 for i=1:6
     for j=1:7
-        if theboard(i,j)==0
+        if board(i,j)==0
             board(i,j)='*';
         else
             board(i,j)=char(board(i,j));
