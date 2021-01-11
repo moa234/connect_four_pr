3b10c3e87c857f5d59673c93d9f63295ab6b14e2
moa234 <64367134+moa234@users.noreply.github.com>
2021-01-11 15:09:46 +0200

    Update board_full.m

--- board_full.m
+++ board_full.m
@@ -1,19 +1,14 @@
 function full=board_full(board)
 %this function checks if the board is full or not
-%made by Abdelrahman Hamdy 
+%made by Abdelrahman Hamdy
 %DATE 10\1\2020
 
 full = 0;
 
-    for col=1:7
-        if board(1,col)==0
-        break
-            %the board is not full
-           full=0;
-        else
-           full=1;
-            %the board is full
-            %DRAW GAME
-        end
-    end
+v = board(1,:);
+if all(v)
+    full=1;
+else
+    full=0;
+end
 end
