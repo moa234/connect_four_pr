dd45c16606983a1dfa1223882691866893967913
moa234 <64367134+moa234@users.noreply.github.com>
2021-01-11 14:49:09 +0200

    Update board_full.m

--- board_full.m
+++ board_full.m
@@ -3,11 +3,13 @@ function full=board_full(board)
 %made by Abdelrahman Hamdy 
 %DATE 10\1\2020
 
+full = 0;
+
     for col=1:7
         if board(1,col)==0
         break
             %the board is not full
-           full=0
+           full=0;
         else
            full=1;
             %the board is full
