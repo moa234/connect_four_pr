693c22bf37148ad951203bffa8fb80b30d3c5b52
engyahmednasr <77208180+engyahmednasr@users.noreply.github.com>
2021-01-12 13:49:20 +0200

    Update wheredropping.m

--- wheredropping.m
+++ wheredropping.m
@@ -8,13 +8,13 @@ function chosencolumn=wheredropping(table, playerplaying, playercolour)
 fprintf('%d''s turn now with colour %c\n',playerplaying,playercolour)
 %next is where he will drop
 chosencolumn=input('Enter a column number you wish to drop in: ');
-while chosencolumn < 1 || chosencolumn > 7
+while chosencolumn ~= [1:7]
     chosencolumn=input('Enter a column number between 1 & 7 please: ');
 end
 %next is checking if the column he/she chose is already full
 while table(1, chosencolumn)==1 || table(1, chosencolumn)==2
     chosencolumn=input('This column is full please enter another column number: ');
-    while chosencolumn < 1 || chosencolumn > 7
+    while chosencolumn ~= [1:7]
         chosencolumn=input('Enter a column number between 1 & 7 please: ');
     end
 end
