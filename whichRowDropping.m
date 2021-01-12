56b6757d05322382043ce04376f31f5119bdd628
engyahmednasr <77208180+engyahmednasr@users.noreply.github.com>
2021-01-12 08:11:09 +0200

    Update whichRowDropping.m

--- whichRowDropping.m
+++ whichRowDropping.m
@@ -3,12 +3,12 @@ function table = whichRowDropping ( table , player , chosencolumn )
 %made by : diana tarek 
 %code: 1200934
 %email: diana.akrab01@eng-st.cu.edu.eg
-% this function used for find the row drop and received the form of table ,
-% playercolour ,and number of column that player chosen. And return the
-% form of table after edit
+% this function used to find the row drop and receive the form of table ,
+% playercolour ,and the number of column that player chosen. And return the
+% form of table after editing
 
 for r = 6 : -1 : 1
-    % Used this loop for knowing the number of row that the coin will put
+    % Used this loop to determine the number of row at which the coin is dropping
     if table( r , chosencolumn ) ~= 1 && table( r , chosencolumn ) ~= 2 
         % then take the player colour and store it in the matrix (table)
         table( r , chosencolumn ) = player;
