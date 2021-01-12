f1daf50b762ddf5892185ce0db70f0584200a21e
moa234 <64367134+moa234@users.noreply.github.com>
2021-01-12 14:15:27 +0200

    Update connect4.m

--- connect4.m
+++ connect4.m
@@ -77,7 +77,7 @@ while 1   % make an infinite loop that exits only when breaked
             break;
         else
             board = restarting(board);  % reset the board
-            mode = input('Do you wish to player against a friend or a computer (P for player and C for computer): ','s');
+            mode = input('Do you wish to player against a friend or a computer (p for player and c for computer): ','s');
             while mode ~= 'c' && mode ~= 'p'   % asks for the mode
                 mode = input('enter either c to play against a computer or p to play againt a friend: ','s');
             end
